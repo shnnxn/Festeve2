2951db173f7463d8c71d7baeddd60c7c7083483a
shnnxn <fmtalkk@gmail.com>
2016-10-24 02:36:12 +0530

    fork it . starting ui

---
+++ app/src/main/java/com/example/android/festeve/create_fest.java
@@ -0,0 +1,13 @@
+package com.example.android.festeve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class create_fest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_fest);
+    }
+}
