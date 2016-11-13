56689a94e569c5b8463cc694221a76c5251ff70f
shnnxn <fmtalkk@gmail.com>
2016-11-13 22:19:14 +0530

    fork it . starting ui

---
+++ app/src/main/java/com/example/android/festeve/fest_feeds.java
@@ -0,0 +1,13 @@
+package com.example.android.festeve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fest_feeds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fest_feeds);
+    }
+}
