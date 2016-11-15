4d8d4b28ec051e830afdd976497d6b1ffc99d6e3
divya <diviakothari17@gmail.com>
2016-11-15 09:45:16 +0530

    try

--- app/src/main/java/com/example/android/festeve/FrontActivity.java
+++ app/src/main/java/com/example/android/festeve/FrontActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.festeve;
 
+import android.drm.DrmStore;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class FrontActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,6 @@ public class FrontActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
 
     @Override
     public void onBackPressed() {
@@ -76,13 +80,13 @@ public class FrontActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem) item; {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.signin) {
+        if (id == R.id.actionBarSignIn) {
 
-        } else if (id == R.id.intrested) {
+        } else if (id == R.id.Intrested) {
 
         } else if (id == R.id.register) {
 
@@ -98,4 +102,8 @@ public class FrontActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
+
 }
