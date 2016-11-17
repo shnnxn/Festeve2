a1dd042e8fd073d44b8fc3ce26db1dfc05ee3d06
divya <diviakothari>
2016-11-17 08:53:40 +0530

    intent added to all front activity options and 3 new activites added

--- app/src/main/java/com/example/android/festeve/create_fest.java
+++ app/src/main/java/com/example/android/festeve/create_fest.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class create_fest extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_fest);
+
+
+
+
     }
 }
